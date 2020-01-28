2f5dd7c521b8c385d1761d258867ffba998a5e55
Alex Kulagin <al.qlagin@gmail.com>
2020-01-28 19:15:34 +0500

    export component with public-api.ts

--- projects/ngx-pwa-install/src/public-api.ts
+++ projects/ngx-pwa-install/src/public-api.ts
@@ -3,4 +3,5 @@
  */
 
 export { NgxPwaInstallModule } from './lib/ngx-pwa-install.module';
+export { NgxPwaInstallComponent } from './lib/ngx-pwa-install.component';
 export { BeforeInstallPrompt } from './lib/ngx-pwa-install.providers';
