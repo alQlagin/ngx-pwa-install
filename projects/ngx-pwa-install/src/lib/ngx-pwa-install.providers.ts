b513b582769cbacd68b26cb43ad2187e46d0491c
Alex Kulagin <al.qlagin@gmail.com>
2020-01-28 17:00:57 +0500

    add generic type for BeforeInstallPrompt token

--- projects/ngx-pwa-install/src/lib/ngx-pwa-install.providers.ts
+++ projects/ngx-pwa-install/src/lib/ngx-pwa-install.providers.ts
@@ -1,17 +1,21 @@
 import { InjectionToken } from '@angular/core';
 import { fromEvent, ReplaySubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { BeforeInstallPromptEvent } from './before-install-prompt.event';
 
 export const beforeInstallPromptFactory = () => {
-  const event$ = fromEvent<Event>(window, 'beforeinstallprompt')
+  const event$ = fromEvent<BeforeInstallPromptEvent>(window, 'beforeinstallprompt')
     .pipe(
       tap(e => e.preventDefault()),
     );
-  const beforeInstallPromptSubject = new ReplaySubject<Event>();
+  const beforeInstallPromptSubject = new ReplaySubject<BeforeInstallPromptEvent>();
   event$.subscribe(beforeInstallPromptSubject);
   return beforeInstallPromptSubject;
 };
-export const BeforeInstallPrompt = new InjectionToken<ReplaySubject<Event>>('BeforeInstallPrompt', {
-  providedIn: 'root',
-  factory: beforeInstallPromptFactory
-});
+export const BeforeInstallPrompt = new InjectionToken<ReplaySubject<BeforeInstallPromptEvent>>(
+  'BeforeInstallPrompt',
+  {
+    providedIn: 'root',
+    factory: beforeInstallPromptFactory
+  }
+);
